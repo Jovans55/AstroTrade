d85b7c4979469b3fbf6426e3346e3bdc093815d3
Jovan Stosic <jovanstosic012@gmail.com>
2023-05-30 15:55:17 -0500

    Added lots of styling a navbar a background and title

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ import './App.css'
 function App() {
 
   return (
-    <div>TEST!</div>
+    <>
+    <div>
+      <h1 className={"title"}>Aᓭℸ ̣ ∷𝙹t∷ᔑ↸ᒷ</h1>
+    </div>
+    <nav>
+      <ul>
+        <li><a>H𝙹ᒲᒷ</a></li>
+        <li><a>Nᒷ∴ᓭ</a></li>
+        <li><a>C𝙹リℸ ̣ ᔑᓵℸ ̣</a></li>
+        <li style={{ float: "right", paddingRight: "15px" }}><a><img src="https://cdn-icons-png.flaticon.com/512/60/60992.png" height={"25px"} width={"25px"} /></a></li>
+      </ul>
+    </nav>
+    </>
   )
 }
 
