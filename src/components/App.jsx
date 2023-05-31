20803d2f025bd936ee640ddc3b026f1dd8821c1f
Jovan Stosic <jovanstosic012@gmail.com>
2023-05-30 19:49:24 -0500

    Changed up some colors biggest change over all is getting rid of the list for divs to be able to make the nav bar look amazing and work amazing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,21 @@ function App() {
   return (
     <>
     <div>
-      <h1 className={"title"}>Aᓭℸ ̣ ∷𝙹t∷ᔑ↸ᒷ</h1>
+      <h1 id={"title"}>Aᓭℸ ̣ ∷𝙹t∷ᔑ↸ᒷ</h1>
     </div>
-    <nav>
-      <ul>
-        <li><a>H𝙹ᒲᒷ</a></li>
-        <li><a>Nᒷ∴ᓭ</a></li>
-        <li><a>C𝙹リℸ ̣ ᔑᓵℸ ̣</a></li>
-        <li id={"search"}><input placeholder='🔍ᓭᒷᔑ∷ᓵ⍑'></input><button>ᒷリℸ ̣ ᒷ∷</button></li>
-        <li id={"cart"}><a><img src="https://cdn-icons-png.flaticon.com/512/60/60992.png" height={"23px"} width={"23px"} /></a></li>
-      </ul>
+    <nav id={"navBar"}>
+      <div id={'tabs'}>
+        <a>H𝙹ᒲᒷ</a>
+        <a>Nᒷ∴ᓭ</a>
+        <a>C𝙹リℸ ̣ ᔑᓵℸ ̣</a>
+      </div>
+      <div id={'search'}>
+        <input placeholder='🔍ᓭᒷᔑ∷ᓵ⍑'></input>
+        <button>ᒷリℸ ̣ ᒷ∷</button>
+      </div>
+      <div id={'cart'}>
+        <a><img src="https://cdn-icons-png.flaticon.com/512/60/60992.png" height={"25px"} width={"25px"} /></a>
+      </div>
     </nav>
     </>
   )
