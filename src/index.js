4edf8037ba8655429e1130c3ad9ed746d8a1d2c1
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-01 13:21:12 -0500

    Added redux into the app and a store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import store from "./store/index";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
