4ec078f2f62c154d27d0b08e4106f3542e440b54
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-01 13:34:59 -0500

    Added the productslice to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import productSlice from "./productSlice";
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    products: productSlice,
+  },
 });
 
 export default store;
