4b1af316bee9fd11f11a2c6d95d073d0c971bb45
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-01 17:34:46 -0500

    Adding some fake data in a js file to seed

--- seed.js
+++ seed.js
@@ -2,30 +2,13 @@ const { db } = require("./server/db");
 
 const Product = require("./server/db/models/Product");
 
-const prodcutsData = [
-  {
-    name: "test",
-    price: 20.0,
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLrH76GDBxZiFUKopDcteBazOkq8MIRoZvWQ&usqp=CAU",
-    description: "HELLOO IA AM JOVAN SOTSI THIS IS UNCIORN GLUE",
-    popular: true,
-  },
-  {
-    name: "test2",
-    price: 2033.44,
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLrH76GDBxZiFUKopDcteBazOkq8MIRoZvWQ&usqp=CAU",
-    description: "HELLOO IA AM JOVAN SOTS GA GHHAHAHH THIS IS NOT It fake GLUE",
-    popular: false,
-  },
-];
+const productsData = require("./productsData");
 
 const seed = async () => {
   try {
     await db.sync({ force: true });
     const products = await Promise.all(
-      prodcutsData.map((product) => {
+      productsData.map((product) => {
         return Product.create(product);
       })
     );
