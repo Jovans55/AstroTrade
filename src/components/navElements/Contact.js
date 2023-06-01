da7071a656255b28b33ab7a4d8d191e8757c1734
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-01 18:26:07 -0500

    Made the contact page, added an image and some text to it

--- src/components/navElements/Contact.js
+++ src/components/navElements/Contact.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Contact = () => {
-  return <aside style={{ color: "white" }}>630863</aside>;
+  return (
+    <section>
+      <h1>How to contact us!</h1>
+      <img
+        style={{ border: "5px solid black", margin: "25px" }}
+        src={process.env.PUBLIC_URL + "contact.jpg"}
+        alt="crazy man trying to find aliens with a radio"
+        width={"1000px"}
+      />
+      <h1>Stare up at the sky and wait for a while we'll pop up eventually</h1>
+    </section>
+  );
 };
 
 export default Contact;
