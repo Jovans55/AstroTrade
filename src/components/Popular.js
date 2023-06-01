03fecf2e292cbe8ab64846c5e80a4b557da8e30c
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-01 15:24:48 -0500

    Testing out some card deisgns and formats

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./Card.css";
 
 const Popular = () => {
   const [backendData, setBackendData] = useState([]);
@@ -10,9 +11,28 @@ const Popular = () => {
       });
   }, []);
 
-  console.log("HELLO1", backendData);
+  const popularProducts = backendData.filter((product) => product.popular);
 
-  return <section id={"popular"}>hi</section>;
+  console.log(popularProducts);
+
+  return (
+    <section id={"popular"}>
+      {popularProducts.map((product) => (
+        <div class="myCard">
+          <div class="innerCard">
+            <div class="frontSide">
+              <img src={product.imageUrl} width={"150px"} alt="trash" />
+              <p class="title">{product.name}</p>
+            </div>
+            <div class="backSide">
+              <p class="title">{product.description}</p>
+              <p>Leave Me</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Popular;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
+  const [backendData, setBackendData] = useState([]);
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
+  console.log("products", backendData);
+
   return <section id={"products"}>Products</section>;
 };
 
