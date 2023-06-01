190137eeea9c749b8b5de0f84954ef8b601f07ab
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-01 13:33:14 -0500

    Created the product slice

---
+++ src/store/productSlice.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+export const fetchProductsAsync = createAsyncThunk("products", async () => {
+  try {
+    const { data } = await axios.get("/api/campuses");
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+});
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
+});
+
+export const selectProducts = (state) => {
+  return state.products;
+};
+
+export default productSlice.reducer;
