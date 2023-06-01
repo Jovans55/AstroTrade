beb8d0f75bb7aaac822fbf0ea7d7cb27c8dd0020
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-01 13:27:50 -0500

    Added the api calls and correct routes to get all products

---
+++ server/api/index.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const Products = require("../db/models/Product");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Products.findAll();
+    res.send(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((req, res, next) => {
+  const error = new Error("API route not found!");
+  error.status = 404;
+  next(error);
+});
+
+module.exports = router;
