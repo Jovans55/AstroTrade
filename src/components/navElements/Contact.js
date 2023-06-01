87225171317f4d38150ee0441841f81c8febe6f7
Jovan Stosic <jovanstosic012@gmail.com>
2023-06-01 18:32:05 -0500

    Added a news section that's just for fun might add more to both contact and news later

--- src/components/navElements/Contact.js
+++ src/components/navElements/Contact.js
@@ -3,14 +3,16 @@ import React from "react";
 const Contact = () => {
   return (
     <section>
-      <h1>How to contact us!</h1>
+      <h1 style={{ color: "white" }}>How to contact us!</h1>
       <img
         style={{ border: "5px solid black", margin: "25px" }}
         src={process.env.PUBLIC_URL + "contact.jpg"}
         alt="crazy man trying to find aliens with a radio"
         width={"1000px"}
       />
-      <h1>Stare up at the sky and wait for a while we'll pop up eventually</h1>
+      <h1 style={{ color: "white" }}>
+        Stare up at the sky and wait for a while we'll pop up eventually
+      </h1>
     </section>
   );
 };
--- src/components/navElements/News.js
+++ src/components/navElements/News.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const News = () => {
-  return <aside style={{ color: "white" }}>News</aside>;
+  return (
+    <section>
+      <h1 style={{ color: "white" }}>News from us blowing your stuff up!</h1>
+      <img
+        style={{ border: "5px solid black", margin: "25px" }}
+        src={process.env.PUBLIC_URL + "news.jpg"}
+        alt="crazy man trying to find aliens with a radio"
+        width={"1000px"}
+      />
+      <h1 style={{ color: "white" }}>Alien 1 Humans 0</h1>
+    </section>
+  );
 };
 
 export default News;
